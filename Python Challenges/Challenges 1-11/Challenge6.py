d5805140d616ef805c269b149bef78d062dcfea4
danielpopovici7 <dpopovici601@gmail.com>
2020-12-30 07:00:23 +0000

    Added new folder for python challenges.

---
+++ Python Challenges/Challenges 1-11/Challenge6.py
@@ -0,0 +1,6 @@
+numOfSlices = float(input("How many slices of pizza did you start with?"))
+
+numOfSlicesEaten = float(input("How many slices of pizza have you eaten?"))
+
+numOfSlicesLeft = numOfSlices - numOfSlicesEaten
+print("Well it looks like you have " + numOfSlicesLeft +" for me!")
